f3e357a71f1701cbe467b77da15053fa869383a4
awais-ur-rehman <awaisjarral37@gmail.com>
2026-01-26 14:38:57 +0500

    added: post interaction functionality; likes and comments

--- lib/shared/models/comment.g.dart
+++ lib/shared/models/comment.g.dart
@@ -10,16 +10,20 @@ _$CommentImpl _$$CommentImplFromJson(Map<String, dynamic> json) =>
     _$CommentImpl(
       id: json['_id'] as String,
       postId: json['post_id'] as String,
-      author: User.fromJson(json['author_id'] as Map<String, dynamic>),
+      author: User.fromJson(
+        _readAuthor(json, 'author') as Map<String, dynamic>,
+      ),
       text: json['text'] as String,
       createdAt: DateTime.parse(json['created_at'] as String),
+      isPending: json['isPending'] as bool? ?? false,
     );
 
 Map<String, dynamic> _$$CommentImplToJson(_$CommentImpl instance) =>
     <String, dynamic>{
       '_id': instance.id,
       'post_id': instance.postId,
-      'author_id': instance.author,
+      'author': instance.author,
       'text': instance.text,
       'created_at': instance.createdAt.toIso8601String(),
+      'isPending': instance.isPending,
     };
